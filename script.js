721a3d1549009d468f6ba6e2500f2a7c10468877
rileymci <riley.mcilwain@connect.qut.edu.au>
2024-01-10 11:52:16 +1000

    Game winning logic working

--- script.js
+++ script.js
@@ -26,7 +26,15 @@ function gameBoard() {
         console.log(boardWithCellValues);
     };
 
-    return {getBoard, placeMarker, printBoard};
+    const resetBoard = () => {
+        for (let i = 0; i < rows; i++){
+            for(let j = 0; j < columns; j++){
+                board[i][j].resetValue();
+            }
+        }
+    }
+
+    return {getBoard, placeMarker, printBoard, resetBoard};
 
 }
 
@@ -39,19 +47,45 @@ function Cell() {
     }
 
     const getValue = () => value;
+    const resetValue = () => {
+        value = "";
+    }
 
-    return {addMarker, getValue};
+    return {addMarker, getValue, resetValue};
 }
 
 function checkWinner(gameBoard){
-    const rows = gameBoard.getBoard().map((row) => row.map((cell) => cell.getValue()));
-    const columns = gameBoard.getBoard().map((column) => column.map((cell) => cell.getValue()));
-
-    console.log(rows.slice(0,1));
-    console.log(columns);
     
+    const boardWithCellValues = gameBoard.getBoard().map((row) => row.map((cell) => cell.getValue()))
+
+    //Check rows
+    for (let i = 0; i < 3; i++){
+        if(boardWithCellValues[i][0] === boardWithCellValues[i][1] && boardWithCellValues[i][0] === boardWithCellValues[i][2] && boardWithCellValues[i][0] !== ''){
+            console.log("true on row iteration: " + i);
+            return true;
+        }
+    }
 
 
+    //Check columns
+    for (let j = 0; j < 3; j++){
+        if(boardWithCellValues[0][j] === boardWithCellValues[1][j] && boardWithCellValues[0][j] === boardWithCellValues[2][j] && boardWithCellValues[0][j] !== ''){
+            console.log("True on column iteration: " + j);
+            return true;
+        }
+    }
+    
+    //Check Diagonals
+    if(boardWithCellValues[0][0] === boardWithCellValues[1][1] && boardWithCellValues[0][0] === boardWithCellValues[2][2] && boardWithCellValues[0][0] !== ''){
+        console.log("Diagonal!");
+        return true;
+    }
+
+    if(boardWithCellValues[0][2] === boardWithCellValues[1][1] && boardWithCellValues[0][2] === boardWithCellValues[2][2] && boardWithCellValues[0][2] !== ''){
+        return true;
+    }
+
+    return false;
     
 }
 
@@ -90,10 +124,17 @@ function gameController(
         console.log(`Dropping ${getActivePlayer().name}'s token into place...`);
         board.placeMarker(row,column,getActivePlayer().token);
 
-        checkWinner(board);
+        if(checkWinner(board)){
+            console.log(`${getActivePlayer().name} Wins!`);
+            resetGame();
+        };
         switchPlayerTurn();
         printNewRound();
     };
+
+    const resetGame = () => {
+        board.resetBoard();
+    }
 
     printNewRound();
 
@@ -103,7 +144,17 @@ function gameController(
 
 const game = gameController();
 
+game.playRound(1,2);
+game.playRound(0,1);
+game.playRound(2,1);
+game.playRound(1,0);
+game.playRound(2,2);
+game.playRound(2,0);
+game.playRound(1,1);
+game.playRound(0,2);
 game.playRound(0,0);
+
+
 
 
 
